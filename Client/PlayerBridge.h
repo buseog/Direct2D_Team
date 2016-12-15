e3e3c3b871a17dc64a33f3ee31f4ed7bb2609ea2
buseog <buseog1991@naver.com>
2016-12-15 16:21:04 +0900

    재업로드

---
+++ Client/PlayerBridge.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "objbridge.h"
+
+class CPlayerBridge :
+	public CObjBridge
+{
+private:
+	list<int>		m_vecBestList;
+
+protected:
+	virtual void	WorldMatrix(INFO& rInfo);
+
+private:
+	void	SetAstar(D3DXVECTOR3 vMouse);
+	void	KeyInput(INFO& rInfo);
+
+public:
+	virtual HRESULT	Initialize(void);
+	virtual void	Progress(INFO& rInfo);
+	virtual void	Render(void);
+	virtual void	Release(void);
+
+public:
+	CPlayerBridge(void);
+	virtual ~CPlayerBridge(void);
+};
